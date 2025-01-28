79fdbf5a64d96061a57f0b5414291131ef465967
Tish <tishthegoat@gmail.com>
2025-01-27 20:20:07 -0700

    menu sheet

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { ShoppingCart, UserIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-import { Button } from "@/components/ui/button"
 import { APP_NAME } from "@/lib/constants"
-import Modetoggle from "./Mode-Toggle"
+import Menu from "./Menu"
 
 
 const Header = () => {
@@ -23,21 +21,7 @@ const Header = () => {
 					</Link>
 				</div>
 
-				<div className="space-x-2">
-					<Modetoggle />
-					<Button variant={'ghost'} asChild>
-						<Link href="/cart">
-							<ShoppingCart />
-							Cart
-						</Link>
-					</Button>
-					<Button  asChild>
-						<Link href="/sign-in">
-							<UserIcon />
-							Sign In
-						</Link>
-					</Button>
-				</div>
+					<Menu />
 			</div>
 		</header>
 	)
