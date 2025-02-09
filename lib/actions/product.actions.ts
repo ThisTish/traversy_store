e4d250878507ded1ecb8527b7973a210743f4772
Tish <tishthegoat@gmail.com>
2025-02-09 03:19:47 -0700

    product page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -5,13 +5,32 @@ import { convertToPlainObject } from "../utils"
 import { LATEST_PRODUCTS_LIMIT } from "../constants"
 
 // get latest products
-
 export async function getLatestProducts() {
-	const data = await prisma.product.findMany({
-		take: LATEST_PRODUCTS_LIMIT,
-		orderBy: {
-			createdAt: 'desc'
-		}
-	})
-	return convertToPlainObject(data)
+	try {
+		const data = await prisma.product.findMany({
+			take: LATEST_PRODUCTS_LIMIT,
+			orderBy: {
+				createdAt: 'desc'
+			}
+		})
+		return convertToPlainObject(data)
+	} catch (error) {
+		console.error("Error fetching latest products:", error)
+		throw new Error("Could not find latest products")
+	}
+}
+
+// get product by slug
+
+export async function getProductBySlug(slug: string){
+	try {
+		return await prisma.product.findFirst({
+			where: {
+				slug
+			}
+		})
+	} catch (error) {
+		console.error('Error fetching product by slug:', error)
+		throw new Error('Could not find product.')
+	}
 }
