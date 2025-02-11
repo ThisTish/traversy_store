313803a3b0590b2fa73ee12228ad4cc6c23196bf
Tish <tishthegoat@gmail.com>
2025-02-11 04:51:05 -0700

    sign in form w/callback functionality

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+
+import '@/app/globals.css'
+import type { Metadata } from "next"
+
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants/index"
+
+export const metadata: Metadata = {
+	title: {
+		template: `%s | ${APP_NAME}`,
+		default: APP_NAME
+	},		
+	description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL)
+}
+
+export default function AuthLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<>
+			<div className="flex-center min-h-screen w-full">
+					{children}
+			</div>
+			</>
+	)
+}
